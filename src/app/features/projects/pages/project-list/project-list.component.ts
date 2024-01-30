8efa42baa68e330973e3df8a9d687ba058307c7a
Kristina Trifunovic <kristina.trifunovic@eng.it>
2024-01-30 12:59:13 +0100

    added filter project form

--- src/app/features/projects/pages/project-list/project-list.component.ts
+++ src/app/features/projects/pages/project-list/project-list.component.ts
@@ -1,6 +1,14 @@
 import { Component, OnDestroy, OnInit } from '@angular/core';
+import {
+  FormBuilder,
+  FormControl,
+  FormGroup,
+  Validators,
+} from '@angular/forms';
 import { Subject, takeUntil } from 'rxjs';
+import { Employee } from 'src/app/core/models/Employee.model';
 import { Project } from 'src/app/core/models/Project.model';
+import { EmployeeService } from 'src/app/core/services/employee.service';
 import { ProjectService } from 'src/app/core/services/project.service';
 
 @Component({
@@ -10,12 +18,22 @@ import { ProjectService } from 'src/app/core/services/project.service';
 })
 export class ProjectListComponent implements OnInit, OnDestroy {
   projects?: Project[];
+  employees?: Employee[];
   destroy$: Subject<boolean> = new Subject();
+  projectForm?: FormGroup;
+  mode = '';
 
-  constructor(private projectService: ProjectService) {}
+  constructor(
+    private projectService: ProjectService,
+    private fb: FormBuilder,
+    private employeeService: EmployeeService
+  ) {
+    this.buildFilterForm();
+  }
 
   ngOnInit(): void {
     this.loadProjects();
+    this.loadEmployees();
   }
 
   ngOnDestroy(): void {
@@ -30,5 +48,43 @@ export class ProjectListComponent implements OnInit, OnDestroy {
       .subscribe((projects) => {
         this.projects = projects;
       });
+  }
+
+  loadEmployees() {
+    this.employeeService
+      .findAll()
+      .pipe(takeUntil(this.destroy$))
+      .subscribe((employees) => {
+        this.employees = employees;
+      });
+  }
+
+  buildFilterForm() {
+    this.projectForm = this.fb.group({
+      name: ['', [Validators.minLength(2), Validators.maxLength(30)]],
+      budgetFrom: ['', [Validators.min(0)]],
+      budgetTo: ['', [Validators.min(0), this.validateBudgetTo.bind(this)]],
+      employees: [''],
+    });
+  }
+
+  validateBudgetTo(formControl: FormControl) {
+    console.log(formControl);
+
+    const budgetFromValue = this.projectForm?.get('budgetFrom')?.value;
+    const budgetToValue = formControl.value;
+
+    if (
+      budgetFromValue !== null &&
+      budgetToValue !== null &&
+      budgetToValue < budgetFromValue
+    ) {
+      return { invalidBudgetTo: true };
+    }
+    return null;
+  }
+
+  compareEmployee(first?: Employee, second?: Employee): boolean {
+    return first?.id === second?.id;
   }
 }
--- src/app/features/projects/projects.module.ts
+++ src/app/features/projects/projects.module.ts
@@ -4,16 +4,15 @@ import { CommonModule } from '@angular/common';
 import { ProjectsRoutingModule } from './projects-routing.module';
 import { ProjectListComponent } from './pages/project-list/project-list.component';
 import { ProjectFormComponent } from './pages/project-form/project-form.component';
-
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 
 @NgModule({
-  declarations: [
-    ProjectListComponent,
-    ProjectFormComponent
-  ],
+  declarations: [ProjectListComponent, ProjectFormComponent],
   imports: [
     CommonModule,
-    ProjectsRoutingModule
-  ]
+    ProjectsRoutingModule,
+    FormsModule,
+    ReactiveFormsModule,
+  ],
 })
-export class ProjectsModule { }
+export class ProjectsModule {}
